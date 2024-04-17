1ea62ae6a8fba75a28466bafa5337be786bdfe81
Cheds64 <166570993+Cheds64@users.noreply.github.com>
2024-04-17 00:11:49 -0700

    finished editing zigzagoon line

--- src/data/pokemon/egg_moves.h
+++ src/data/pokemon/egg_moves.h
@@ -2171,7 +2171,10 @@ const u16 gEggMoves[] = {
         MOVE_SLEEP_TALK,
         MOVE_ROCK_CLIMB,
         MOVE_SIMPLE_BEAM,
-        MOVE_EXTREME_SPEED),
+        MOVE_EXTREME_SPEED
+        MOVE_PARTING_SHOT,
+        MOVE_QUICK_GUARD,
+        MOVE_KNOCK_OFF),
 #if P_GALARIAN_FORMS
     egg_moves(ZIGZAGOON_GALARIAN,
         MOVE_PARTING_SHOT,
